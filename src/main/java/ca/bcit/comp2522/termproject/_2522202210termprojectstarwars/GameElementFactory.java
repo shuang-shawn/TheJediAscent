2e44483638c0dfb0dcbbcbf5de7ca7857681caf9
Chun Yip Wong <cy.jimmy.wong@gmail.com>
2022-04-03 20:58:51 -0700

    Added card images

--- src/main/java/ca/bcit/comp2522/termproject/_2522202210termprojectstarwars/GameElementFactory.java
+++ src/main/java/ca/bcit/comp2522/termproject/_2522202210termprojectstarwars/GameElementFactory.java
@@ -1,5 +1,6 @@
 package ca.bcit.comp2522.termproject._2522202210termprojectstarwars;
 
+import com.almasb.fxgl.core.View;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.dsl.views.ScrollingBackgroundView;
 import com.almasb.fxgl.entity.Entity;
@@ -7,11 +8,14 @@ import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
 import com.almasb.fxgl.entity.components.IrremovableComponent;
+import com.almasb.fxgl.entity.components.ViewComponent;
 import com.almasb.fxgl.texture.Texture;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 public class GameElementFactory implements EntityFactory {
     @Spawns("background")
@@ -36,32 +40,49 @@ public class GameElementFactory implements EntityFactory {
 
     @Spawns("fCard")
     public Entity fCard(SpawnData data) {
+        Texture cardView = FXGL.getAssetLoader().loadTexture("attack.png");
+        Text cardText = FXGL.getUIFactoryService().newText("ATTACK", Color.WHITE, 20);
+        StackPane cardStack = new StackPane();
+        cardStack.getChildren().addAll(cardView, cardText);
         return FXGL.entityBuilder()
-                .view(new Rectangle(140, 180, Color.WHITE))
+                .view(cardStack)
                 .at(30, 410)
                 .build();
     }
 
     @Spawns("gCard")
     public Entity gCard(SpawnData data) {
+        Texture cardView = FXGL.getAssetLoader().loadTexture("block.png");
+        Text cardText = FXGL.getUIFactoryService().newText("BLOCK", Color.WHITE, 20);
+        StackPane cardStack = new StackPane();
+        cardStack.getChildren().addAll(cardView, cardText);
         return FXGL.entityBuilder()
-                .view(new Rectangle(140, 180, Color.BLUE))
+                .view(cardStack)
                 .at(230, 410)
                 .build();
     }
 
     @Spawns("hCard")
     public Entity hCard(SpawnData data) {
+        Texture cardView = FXGL.getAssetLoader().loadTexture("atkBuff.png");
+        Text cardText = FXGL.getUIFactoryService().newText("INCREASE ATK", Color.WHITE, 20);
+        StackPane cardStack = new StackPane();
+        cardStack.getChildren().addAll(cardView, cardText);
         return FXGL.entityBuilder()
-                .view(new Rectangle(140, 180, Color.GREEN))
+                .view(cardStack)
                 .at(430, 410)
                 .build();
     }
+    //140x180
 
     @Spawns("jCard")
     public Entity jCard(SpawnData data) {
+        Texture cardView = FXGL.getAssetLoader().loadTexture("j.png");
+        Text cardText = FXGL.getUIFactoryService().newText("J", Color.WHITE, 20);
+        StackPane cardStack = new StackPane();
+        cardStack.getChildren().addAll(cardView, cardText);
         return FXGL.entityBuilder()
-                .view(new Rectangle(140, 180, Color.ORANGE))
+                .view(cardStack)
                 .at(630, 410)
                 .build();
     }
